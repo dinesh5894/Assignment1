40da39769c470a07427831f4c444861e833a113e
dinesh5894 <dinesh.bhairi@gmail.com>
2023-07-06 21:25:06 +0530

    assignment1 added

---
+++ assignment1/OddPositon.java
@@ -0,0 +1,18 @@
+package com.assignment1;
+
+
+public class OddPositon {
+
+    public static void main(String[] args) {
+        // Initialized array
+        int inputArray[] = new int[]{103, -500, 450, -200,
+                1000, -213, 750};
+
+        for (int i = 0; i < inputArray.length; i++) {
+            if (i % 2 != 0) {
+                System.out.println(inputArray[i]);
+            }
+        }
+    }
+
+}
