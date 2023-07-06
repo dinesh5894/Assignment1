28dad60ff5730cd6c451f4d4899a51c5c716fd4f
dinesh5894 <dinesh.bhairi@gmail.com>
2023-07-06 20:22:08 +0530

    Assignment added

---
+++ assignment1/ReverseArray.java
@@ -0,0 +1,16 @@
+package com.assignment1;
+
+public class ReverseArray {
+
+        public static void main(String[] args) {
+            int arr[] = {1,2,3,4,5};
+
+            for (int i= arr.length-1;i>=0;i--){
+
+                System.out.println(arr[i]);
+
+            }
+        }
+    }
+
+
